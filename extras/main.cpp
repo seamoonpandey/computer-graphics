01d173c9bfa0b21e48140657aaa06b6a4f24e19b
Seamoon Pandey <hackedasocial@gmail.com>
2024-07-17 23:26:32 +0545

    build the ric body and the side pillars

--- extras/main.cpp
+++ extras/main.cpp
@@ -6,9 +6,12 @@ void init();
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
     glutCreateWindow("RIC building");
-    glutDisplayFunc(display);
     init();
+    glutDisplayFunc(display);
     glutMainLoop();
 
     return 0;
@@ -16,7 +19,7 @@ int main(int argc, char **argv)
 
 void init()
 {
-    glClearColor(1.0, 1.0, 1.0, 1.0); // white background
+    glClearColor(1.0, 1.0, 1.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
@@ -25,12 +28,38 @@ void init()
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Building background
+
     glBegin(GL_POLYGON);
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
+    glColor3f(0.9647, 0.8431, 0.4863); // that yellow color
+    glVertex2f(-0.9, -0.5);
+    glVertex2f(-0.9, 0.5);
+    glVertex2f(0.9, 0.5);
+    glVertex2f(0.9, -0.5);
     glEnd();
+
+    // Left side of the building
+
+    // first pillar
+    glBegin(GL_POLYGON);
+    glColor3f(0.8235, 0.3843, 0.1804);
+    glVertex2f(-0.9, -0.5);
+    glVertex2f(-0.9, 0.5);
+    glVertex2f(-0.7, 0.5);
+    glVertex2f(-0.7, -0.5);
+    glEnd();
+
+    // Right side of the building
+
+    // second pillar
+    glBegin(GL_POLYGON);
+    glColor3f(0.8235, 0.3843, 0.1804);
+    glVertex2f(0.7, -0.5);
+    glVertex2f(0.7, 0.5);
+    glVertex2f(0.9, 0.5);
+    glVertex2f(0.9, -0.5);
+    glEnd();
+
     glFlush();
 }
