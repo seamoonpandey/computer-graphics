f79fddf2b1b25d4d7e79e916e53d9913dc00ac9d
Seamoon Pandey <hackedasocial@gmail.com>
2024-07-17 23:39:20 +0545

    make pillars thin

--- extras/main.cpp
+++ extras/main.cpp
@@ -25,41 +25,33 @@ void init()
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
 }
 
+void drawRectangle(float x1, float y1, float x2, float y2, float r, float g, float b)
+{
+    glColor3f(r, g, b);
+    glBegin(GL_POLYGON);
+    glVertex2f(x1, y1);
+    glVertex2f(x1, y2);
+    glVertex2f(x2, y2);
+    glVertex2f(x2, y1);
+    glEnd();
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Building background
-
-    glBegin(GL_POLYGON);
-    glColor3f(0.9647, 0.8431, 0.4863); // that yellow color
-    glVertex2f(-0.9, -0.5);
-    glVertex2f(-0.9, 0.5);
-    glVertex2f(0.9, 0.5);
-    glVertex2f(0.9, -0.5);
-    glEnd();
+    drawRectangle(-0.9, -0.5, 0.9, 0.5, 0.9647, 0.8431, 0.4863);
 
     // Left side of the building
-
-    // first pillar
-    glBegin(GL_POLYGON);
-    glColor3f(0.8235, 0.3843, 0.1804);
-    glVertex2f(-0.9, -0.5);
-    glVertex2f(-0.9, 0.5);
-    glVertex2f(-0.7, 0.5);
-    glVertex2f(-0.7, -0.5);
-    glEnd();
+    drawRectangle(-0.9, -0.5, -0.85, 0.5, 0.8235, 0.3843, 0.1804);  // First pillar
+    drawRectangle(-0.75, -0.5, -0.70, 0.5, 0.8235, 0.3843, 0.1804); // Second pillar
+    drawRectangle(-0.6, -0.5, -0.55, 0.5, 0.8235, 0.3843, 0.1804);  // Third pillar
 
     // Right side of the building
-
-    // second pillar
-    glBegin(GL_POLYGON);
-    glColor3f(0.8235, 0.3843, 0.1804);
-    glVertex2f(0.7, -0.5);
-    glVertex2f(0.7, 0.5);
-    glVertex2f(0.9, 0.5);
-    glVertex2f(0.9, -0.5);
-    glEnd();
+    drawRectangle(0.55, -0.5, 0.6, 0.5, 0.8235, 0.3843, 0.1804); // First pillar
+    drawRectangle(0.7, -0.5, 0.75, 0.5, 0.8235, 0.3843, 0.1804); // Second pillar
+    drawRectangle(0.85, -0.5, 0.9, 0.5, 0.8235, 0.3843, 0.1804); // Third pillar
 
     glFlush();
 }
